52c334efc8b08cf9d1be078d6bc8d8b329e33a86
Isabel Henrique <contato.isabelhenrique@gmail.com>
2024-04-12 14:01:40 -0300

    Testando classe de dominio - Category

---
+++ src/main/java/com/dev/spring/domain/Category.java
@@ -0,0 +1,51 @@
+package com.dev.spring.domain;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String name;
+
+    public Category() {}
+
+    public Category(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Comparação de objetos pelo conteúdo
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
