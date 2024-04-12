266daa5b6db69d95883251707c459a6a760ce6a1
Isabel Henrique <contato.isabelhenrique@gmail.com>
2024-04-12 15:12:32 -0300

    Criando operacao de Instanciacao

--- src/main/java/com/dev/spring/Application.java
+++ src/main/java/com/dev/spring/Application.java
@@ -1,13 +1,29 @@
 package com.dev.spring;
 
+import com.dev.spring.domain.Category;
+import com.dev.spring.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// Instanciacao dos objetos toda vez que o programa rodar
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
